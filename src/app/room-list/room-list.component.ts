998d517209586f096f649feead658e6b32ffa420
maurilioSousa-Desing <fmaurilio6@gmail.com>
2021-08-26 02:00:58 -0300

    Create room-list.component.ts

---
+++ src/app/room-list/room-list.component.ts
@@ -0,0 +1,44 @@
+import { RoomService } from '../room.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from '../room';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-room-list',
+  templateUrl: './room-list.component.html',
+  styleUrls: ['./room-list.component.css']
+})
+export class RoomListComponent implements OnInit {
+  rooms!: Observable<Room[]>;
+
+  constructor(private roomService: RoomService,
+    private router: Router) { }
+
+  ngOnInit(){
+    this.reloadData();
+  }
+  reloadData() {
+    this.roomService.getRooList();
+  }
+
+  deleteRoom( id: number){
+    this.roomService.deleteRoom(id)
+     .subscribe(
+        data=> {
+          console.log(data)
+          this.reloadData();
+        },
+        (error: any) => console.log(error)
+        );
+      }
+
+      roomDetails( id: number){
+           this.router.navigate(['details', id]);
+      }
+
+      updateRoom( id: number){
+          this.router.navigate(['update',id]);
+      }
+
+}
